b2a0ddd5d941c86c61ac8d8d06b2faaa07e2e4f7
alineazedias <aline.azedias@gmail.com>
2023-12-31 17:42:16 -0300

    site ok2

--- js/script.js
+++ js/script.js
@@ -17,3 +17,12 @@ function cadastro() {
     alert("Cadastrado com sucesso!");
     window.location.href = "index.html";
 }
+
+
+function topo() {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+}
